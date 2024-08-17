298fcbd6d1eefc90171df37f71f751a509ea23e4
SeungYeon <ksh_0403@naver.com>
2024-08-17 16:08:21 +0900

    set: initial setting

---
+++ src/main/java/com/example/Profee/ProfeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Profee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Profee/ProfeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Profee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
